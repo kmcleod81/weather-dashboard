e460924d4da15023b5c396e75d22fe190a977440
kdawg <mcleod.karla@gmail.com>
2020-05-09 17:56:16 -0400

    edited html div's to poulate on the page in the correct odrder. Added for loop for 5 day weather. added units-imperial to my URLs to pull in the ferenheit temp. Added functions to uv index and search weather

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,33 +1,66 @@
 // an input to allow the user to search for the city
 
 // grab the users input and store it in a variable (userSearch)
+var APIKey = "6b7b386acbe114ac13fa0de66207a370";
 
 $(document).ready(function () {
-  var APIKey = "6b7b386acbe114ac13fa0de66207a370";
-  // current city API
-  var weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=goffstown&appid=${APIKey}`;
-  // 5-day forecast API
-  var forecastURL = `https://api.openweathermap.org/data/2.5/forecast?id=goffstown&appid=${APIKey}`;
+  $("#citySearch").on("click", searchWeather);
 
-  // date using moment.js
-  var currentDate = moment().format("l");
+  function searchWeather() {
+    var location = $("#location").val();
+    dailyWeather(location);
+    forecastWeather(location);
+  }
 
-  // create a function that gets the current weather (Current Weather Data Call)
-  $.get(weatherURL).then(function (response) {
-    // console.log(weatherURL);
-    console.log(response);
+  function dailyWeather(location) {
+    // current city API
+    var weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIKey}&units=imperial`;
 
-    // Transfer content to HTML
-    $(".city").html("<h2>" + response.name + " " + currentDate);
-    $(".temp").text("Temperature: " + response.main.temp);
-    $(".humidity").text("Humidity: " + response.main.humidity);
+    // date using moment.js
+    var currentDate = moment().format("l");
 
-    // generate current city and date elements
-    var currentCity = response.name;
-    var currentCityEl = $("<span>", {
-      style: "font-size: 25px",
-    }).text(`${currentCity} (${currentDate}) `);
-  });
+    // create a function that gets the current weather (Current Weather Data Call)
+    $.get(weatherURL).then(function (response) {
+      // console.log(weatherURL);
+      console.log(response);
+      // Transfer content to HTML
+      $(".city").html("<h2>" + response.name + currentDate);
+      $(".wind").text("Wind Speed: " + response.wind.speed);
+      $(".humidity").text("Humidity: " + response.main.humidity);
+      $(".temp").text("Temperature: " + response.main.temp);
+
+      // uv index API
+      var uvIndexURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${response.coord.lat}&lon=${response.coord.lon}&
+ exclude=hourly,daily&appid=${APIKey}`;
+
+      $.get(uvIndexURL).then(function (response) {
+        // Transfer content to HTML
+        $(".uvi").text("UV Index: " + response.current.uvi);
+
+        // generate current city and date elements
+        var currentCity = response.name;
+        var currentCityEl = $("<span>", {
+          style: "font-size: 25px",
+        }).text(`${currentCity} (${currentDate}) `);
+      });
+    });
+  }
+
+  function forecastWeather(location) {
+    // 5-day forecast API
+    var forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${APIKey}&units=imperial`;
+
+    $.get(forecastURL).then(function (response) {
+      // $()
+      for (let index = 0; index < response.list.length; index++) {
+        const element = response.list[index];
+        if (element.dt_txt.indexOf("15:00:00") !== -1) {
+          console.log(element);
+          // populate html for 5 day weather
+        }
+      }
+    });
+  }
 });
 
 // dynamically generate current weather <div>
