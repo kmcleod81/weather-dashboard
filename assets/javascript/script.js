e36da8148cec7f0fd6db2115201bcc5960fa8a2d
kdawg <mcleod.karla@gmail.com>
2020-05-09 07:36:30 -0400

    added moment js to html and script, and 5-day API and added psuedocode

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,7 +3,23 @@
 // grab the users input and store it in a variable (userSearch)
 // create a function that gets the current weather (Current Weather Data Call)
 
-// function(argument)
+$(document).ready(function () {
+  var APIKey = "6b7b386acbe114ac13fa0de66207a370";
+  // current city API
+  var weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=goffstown&appid=${APIKey}`;
+  // 5-day forecast API
+  var forecastURL = `https://api.openweathermap.org/data/2.5/forecast?id=goffstown&appid=${APIKey}`;
+
+  // date using moment.js
+  var currentDate = moment().format("L");
+
+  $.get(weatherURL).then(function (response) {
+    // Log the queryURL
+    console.log(weatherURL);
+    // Log the resulting object
+    console.log(response);
+  });
+});
 
 // dynamically generate current weather <div>
 // inside of that you're going to generate the city, date, tempeature, humidity, wind speed
@@ -20,12 +36,14 @@
 // if the users search exists in local storage, don't append a new one to the searches, just grab from local storage
 // in the getCurrentWeather function, check local storage for previous searches
 
-function(lat, lon) {
-    var apikey =
-        $.get('http://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}', function (res) {
+// var location = document.getElementById("location");
 
-        })
-}
+// function(lat, lon) {
+//   var APIKey = "6b7b386acbe114ac13fa0de66207a370";
+//           $.get('http://api.openweathermap.org/data/2.5/uvi?apikey=${APIKey}', function (res) {
+
+//         })
+// }
 
 // 5 day weather icon
-.icon + ".png"
+// .icon + ".png"
